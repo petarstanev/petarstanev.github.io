cd68489a68c3f0a53d156acd33bb53829d99e6fc
Petar Stanev <petarstanev@outlook.com>
2023-03-28 13:11:46 +0700

    Update resume link for iframe

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -5,7 +5,7 @@ const Resume = forwardRef<HTMLDivElement>((undefined, ref) => {
     <section ref={ref}>
       <h2>Resume</h2>
       <iframe
-        src="https://docs.google.com/gview?url=https://petarstanev.com/Resume%20Petar%20Stanev.pdf&embedded=true"
+        src="https://docs.google.com/gview?url=https://petarstanev.com/Resume_Petar_Stanev.pdf&embedded=true"
         width={"100%"}
         height={"480"}
         className="border my-4 rounded-md border-violet-500 bg-slate-50"
