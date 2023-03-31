2d215e54307be99c9ef110ae66de0f9242d1a271
Petar Stanev <petarstanev@outlook.com>
2023-03-31 19:58:16 +0700

    Change pdf resume to load locally

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -4,14 +4,16 @@ const Resume = forwardRef<HTMLDivElement>((undefined, ref) => {
   return (
     <section ref={ref}>
       <h2>Resume</h2>
+      {/* /resume.pdf#toolbar=0 to hide toolbar */}
       <iframe
-        src="https://docs.google.com/gview?url=https://petarstanev.com/Resume_Petar_Stanev.pdf&embedded=true"
-        width={"100%"}
-        height={"480"}
+        width="100%"
+        height="480"
+        allowFullScreen={true}
+        src="/Resume_Petar_Stanev.pdf"
         className="border my-4 rounded-md border-violet-500 bg-slate-50"
       />
     </section>
   );
 });
-Resume.displayName = 'Resume';
+Resume.displayName = "Resume";
 export default Resume;
